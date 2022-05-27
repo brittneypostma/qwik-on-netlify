3b9dd79776a9a3961564f6d40efb368850a506fd
Brittney Postma <brittneypostma@Brittneys-MacBook-Pro.local>
2022-05-27 09:15:10 -0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+
+import { defineConfig } from 'vite';
+import netlifyEdge from '@netlify/vite-plugin-netlify-edge';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      qwikVite({
+        ssr: { outDir: '.netlify/edge-functions/entry.netlify' },
+      }),
+      netlifyEdge({
+        functionName: 'entry.netlify',
+      }),
+    ],
+  };
+});
